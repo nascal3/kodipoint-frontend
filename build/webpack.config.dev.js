9601ab184998c9163a23dac54072386db394c094
nascal3 <nascal3@gmail.com>
2019-08-04 02:06:56 +0300

    added webpack cong to project

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,143 @@
+const webpack = require("webpack");
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+function resolve(dir) {
+  return path.join(__dirname, "..", dir);
+}
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    publicPath: "/"
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+        options: {
+          data: `@import "${resolve("./src/styles/index.scss")}";`,
+          includePaths: [__dirname, "src"]
+        }
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            compact: true
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]?[hash]"
+        }
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          "vue-style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+              fiber: require("fibers"),
+              indentedSyntax: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "vue-style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              data: `@import "${resolve("./src/styles/main.scss")}";`,
+              includePaths: [__dirname, "src"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    overlay: true,
+    stats: {
+      children: false
+    }
+  },
+  resolve: {
+    extensions: [".js", ".vue", ".scss"],
+    alias: {
+      "@": resolve("src"),
+      vue$: "vue/dist/vue.esm.js"
+    }
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      favicon: "./src/favicon.ico"
+    }),
+    new Dotenv(),
+    new VuetifyLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css"
+    }),
+    new OptimizeCssAssetsPlugin({
+      cssProcessor: require("cssnano"),
+      cssProcessorPluginOptions: {
+        preset: [
+          "default",
+          {
+            discardComments: {
+              removeAll: true
+            }
+          }
+        ]
+      },
+      canPrint: true
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        BASE_URL: JSON.stringify(process.env.BASE_URL)
+      }
+    })
+  ],
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all"
+        }
+      }
+    }
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,164 @@
+const webpack = require('webpack')
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          data: `@import "${resolve('./src/styles/index.scss')}";`,
+          includePaths: [__dirname, 'src']
+        }
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env'
+            ],
+            compact: true
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'vue-style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              fiber: require('fibers'),
+              indentedSyntax: true // optional
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              data: `@import "${resolve('./src/styles/main.scss')}";`,
+              includePaths: [__dirname, 'src'],
+              minimize: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    overlay: true,
+    stats: {
+      children: false
+    }
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.vue',
+      '.scss'
+    ],
+    alias: {
+      '@': resolve('src'),
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './src/favicon.ico'
+    }),
+    new VuetifyLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css'
+    }),
+    new OptimizeCssAssetsPlugin({
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', {
+          discardComments: {
+            removeAll: true
+          }
+        }]
+      },
+      canPrint: true
+    }),
+    new CompressionPlugin(),
+    new BundleAnalyzerPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        BASE_URL: JSON.stringify(process.env.BASE_URL)
+      }
+    })
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    },
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          ecma: 6,
+          compress: true,
+          output: {
+            comments: false,
+            beautify: false
+          }
+        }
+      })
+    ]
+  }
+}
