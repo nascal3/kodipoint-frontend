4274d3f9353447d1135df3044d57f176795a3829
nascal3 <nascal3@gmail.com>
2019-10-05 20:38:50 +0300

    added login background

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,5 @@
 <template>
-    <section>
+    <section class="login">
         login page
     </section>
 </template>
--- src/pages/Summary.vue
+++ src/pages/Summary.vue
@@ -1,14 +1,11 @@
 <template>
-  <HelloWorld />
+  <section></section>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld'
 
 export default {
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
