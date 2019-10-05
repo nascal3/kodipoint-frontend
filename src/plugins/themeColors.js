d337f343f2a3470ad36150b2fe46403ce06d284a
nascal3 <nascal3@gmail.com>
2019-10-05 17:56:57 +0300

    set up theme  colors

---
+++ src/plugins/themeColors.js
@@ -0,0 +1,12 @@
+export default {
+  light: {
+    primary: '#E75125',
+    secondary: '#0E4645',
+    info: '#1867C0',
+    warning: '#f4cf27',
+    error: '#ff0000'
+  },
+  dark: {
+    primary: '#E75125'
+  }
+}
