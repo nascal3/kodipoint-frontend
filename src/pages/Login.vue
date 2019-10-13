5c00cb8ebffd7aa733eb4d0c65ae31e308c19722
nascal3 <nascal3@gmail.com>
2019-10-13 19:09:19 +0300

    setup login section for form

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -5,7 +5,18 @@
             <v-col cols="12">
                 <v-row class="login-body" align="center" justify="center" no-gutters>
                     <v-card class="login-form-container">
-                        stuff
+                        <v-row no-gutters>
+                            <v-col cols="5">
+                                <v-row class="login-left-section" align="center" justify="center" no-gutters>
+                                    <v-img class="login-logo" :src="require('@/assets/images/kodiPoint_logo.png')"></v-img>
+                                </v-row>
+                            </v-col>
+                            <v-col cols="7">
+                                <v-row class="login-right-section" align="center" justify="center" no-gutters>
+                                    <div class="form-header">Sign in</div>
+                                </v-row>
+                            </v-col>
+                        </v-row>
                     </v-card>
                 </v-row>
             </v-col>
