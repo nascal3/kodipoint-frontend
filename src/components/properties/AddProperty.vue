f0113437cd828a4633330b62b703b057d461b64d
nascal3 <nascal3@gmail.com>
2019-11-05 11:03:45 +0300

    added new form fields

--- src/components/properties/AddProperty.vue
+++ src/components/properties/AddProperty.vue
@@ -6,14 +6,14 @@
       <v-form v-model="valid" @submit.prevent="addProperty">
         <v-text-field
           v-model="propertyName"
-          label="Property name"
+          label="Property name*"
           v-validate="'required'"
           name="propertyName"
           :error="errors.has('propertyName')"
         ></v-text-field>
         <transition name="fade">
           <span class="input-error" v-if="errors.has('propertyName')">
-            Please enter property name!
+            Please enter property name
           </span>
         </transition>
         <v-select
@@ -29,64 +29,98 @@
         </v-radio-group>
         <transition name="fade">
           <section v-if="contact === 'other'">
-          <v-text-field
-              v-model="contactPerson"
-              label="Contacts' name"
-              v-validate="'required'"
-              name="propertyName"
-              :error="errors.has('contactPerson')"
-          ></v-text-field>
-          <transition name="fade">
-            <span class="input-error" v-if="errors.has('contactPerson')">
-            Please insert contacts' name!
-          </span>
-          </transition>
-          <v-text-field
-              v-model="contactPhone"
-              label="Contacts' phone number"
-              v-validate="'required|min:17 '"
-              name="contactPhone"
-              persistent-hint
-              :error="errors.has('contactPhone')"
-              data-vv-as="contacts' phone number"
-              v-mask="['+(###) ####-#####']"
-          ></v-text-field>
-          <transition name="fade">
-            <div class="input-error" v-if="errors.has('contactPhone')">
-              {{ errors.first('contactPhone') }}
-            </div>
-          </transition>
-          <transition name="fade">
-            <span class="input-hint" v-if="!contactPhone.length">
-              Phone number format: 254 7123456789
-            </span>
-          </transition>
-        </section>
+            <v-text-field
+                v-model="contactPerson"
+                label="Contacts' name*"
+                v-validate="'required'"
+                name="contactPerson"
+                :error="errors.has('contactPerson')"
+            ></v-text-field>
+            <transition name="fade">
+              <span class="input-error" v-if="errors.has('contactPerson')">
+                Please insert contacts' name
+              </span>
+            </transition>
+            <v-text-field
+                v-model="contactPhone"
+                label="Contacts' phone number*"
+                v-validate="'required|min:17 '"
+                name="contactPhone"
+                persistent-hint
+                :error="errors.has('contactPhone')"
+                data-vv-as="contacts' phone number"
+                v-mask="['(+###) ####-#####']"
+            ></v-text-field>
+            <transition name="fade">
+              <div class="input-error" v-if="errors.has('contactPhone')">
+                {{ errors.first('contactPhone') }}
+              </div>
+            </transition>
+          </section>
         </transition>
         <v-text-field
             v-model="lrNumber"
-            label="LR number"
+            label="LR number*"
             v-validate="'required'"
             name="lrNumber"
             :error="errors.has('lrNumber')"
         ></v-text-field>
         <transition name="fade">
           <span class="input-error" v-if="errors.has('lrNumber')">
-            Please enter LR Number!
+            Please enter LR Number
           </span>
         </transition>
         <v-text-field
             v-model="nosUnits"
-            label="Number of units"
-            v-validate="'required'"
+            label="Number of units*"
+            v-validate="'required|max:4'"
             name="nosUnits"
+            data-vv-as="number of units"
+            v-mask="['####']"
             :error="errors.has('nosUnits')"
         ></v-text-field>
         <transition name="fade">
           <span class="input-error" v-if="errors.has('nosUnits')">
-            Please enter number of units!
+            {{ errors.first('nosUnits') }}
           </span>
         </transition>
+        <v-textarea
+            name="description"
+            label="Property description"
+            v-model="description"
+        ></v-textarea>
+        <v-combobox multiple
+          v-model="services"
+          label="Property services"
+          append-icon
+          chips
+          deletable-chips
+          class="tag-input"
+          :search-input.sync="search"
+          @keyup.tab="updateTags"
+          @paste="updateTags">
+        </v-combobox>
+        <span class="input-hint">
+          After each service typed press enter button.
+        </span>
+        <v-file-input
+          prepend-icon="mdi-camera"
+          :rules="UploadImageRules"
+          show-size
+          accept="image/*"
+          label="Property Image"
+        >
+        </v-file-input>
+        <v-btn
+          type="submit"
+          :loading="showLoader"
+          :disabled="showLoader"
+          class="btn-text"
+          block
+          color="primary"
+        >
+          Save property
+        </v-btn>
       </v-form>
     </v-card-text>
   </v-card>
@@ -103,17 +137,47 @@ export default {
     contactPhone: '',
     lrNumber: '',
     nosUnits: '',
+    description: '',
+    services: ['garbage collection', 'water', 'security'],
+    items: [],
+    search: '',
     propertyType: 'Apartments',
     properties: [
       'Apartments',
       'Business premises',
       'Apartments & Business premises',
       'Warehouse'
+    ],
+    UploadImageRules: [
+      value => !value || value.size < 2000000 || 'Image size should be less than 2 MB!'
     ]
   }),
   methods: {
-    addProperty () {
-      alert('ola')
+    updateTags () {
+      this.$nextTick(() => {
+        this.services.push(...this.search.split(','))
+        this.$nextTick(() => {
+          this.search = ''
+        })
+      })
+    },
+    async addProperty () {
+      const params = {
+        'property_name': this.propertyName,
+        'contact_person': this.contactPerson,
+        'phone': this.contactPhone,
+        'lr_nos': this.lrNumber,
+        'nos_units': this.nosUnits,
+        'description': this.description,
+        'property_services': this.services,
+        'property_type': this.propertyType
+      }
+      try {
+        await this.$validator.validateAll()
+        console.log('ola', params)
+      } catch (e) {
+        throw new Error(e.message)
+      }
     }
   }
 }
